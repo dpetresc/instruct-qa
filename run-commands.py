e8c9553037eb32bc78c477bfef306b2fb184a89e
dpetresc <diana.petrescu@epfl.ch>
2024-05-15 08:47:18 +0000

    run-commands.py

--- run-commands.py
+++ run-commands.py
@@ -3,20 +3,19 @@ import subprocess
 def run_script():
     python_exec_path = '/home/dpetresc/.conda/envs/instruct-qa-venv/bin/python3'
     output_file = "/home/dpetresc/instruct-qa/examples/data/nq/index/cagra/output.log"
-    command = f"""
-    su - dpetresc -c '
-    cd instruct-qa/examples/ && 
-    {python_exec_path} get_started_cagra.py > {output_file} 2>&1
-    '
-    """
-    
-    try:
-        subprocess.run(command, shell=True, check=True)
-        print("Script executed and output redirected to", output_file)
-    except subprocess.CalledProcessError as e:
-        print("Error executing the command:", str(e))
-        print("Return code:", e.returncode)
-        print("Command output:", e.output)
+    script_path = "/home/dpetresc/instruct-qa/examples/get_started_cagra.py"
+    command = [
+        'sudo', '-u', 'dpetresc', python_exec_path, script_path
+    ]
+
+    with open(output_file, "w") as output:
+        try:
+            subprocess.run(command, stdout=output, stderr=output, check=True)
+            print("Script executed and output redirected to", output_file)
+        except subprocess.CalledProcessError as e:
+            print("Error executing the command:", str(e))
+            print("Return code:", e.returncode)
 
 if __name__ == "__main__":
     run_script()
+
